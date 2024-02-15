cefe8e217d283e204031b5000a2c011674dd9462
Rahul Ajnar <rahulajnar42@gmail.com>
2024-02-15 13:20:21 +0530

    Add new line

---
+++ mini-project/src/SearchBox.jsx
@@ -0,0 +1,19 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+
+export default function SearchBox() {
+    return(
+    <div className = "SearchBox">
+        <h3>Search for the weather </h3>
+        <form>
+        <TextField id="city" label="City Name" variant="outlined" required />
+        <br>
+        </br>
+        <Button variant="contained" type="submit">Search </Button>
+        </form>
+  
+       
+    </div>
+        );
+}
